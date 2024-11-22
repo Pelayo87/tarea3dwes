f9480b51737a633461df8f82200ee782041a37cf
Pelayo Rodriguez Alvarez <rodriguezalvarezpelayo30@gmail.com>
2024-11-22 11:04:48 +0100

    Commit inicial

---
+++ tarea3dwes/src/main/java/com/pelayora/tarea3dwes/Principal.java
@@ -0,0 +1,21 @@
+package com.pelayora.tarea3dwes;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+
+public class Principal implements CommandLineRunner {
+
+//	@Autowired
+	
+	
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("INICIO");
+		
+		System.out.println("------------------------");
+		
+		
+		System.out.println("FIN");
+	}
+
+}
