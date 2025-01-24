3ca592fd34017db84b12a72f1578f83a16ee03b5
Pelayo Rodriguez Alvarez <rodriguezalvarezpelayo30@gmail.com>
2025-01-24 13:10:25 +0100

    Se ejecuta el primer html con el localhost

--- tarea3dwes/src/main/java/com/pelayora/tarea3dwes/controlador/ViveroController.java
+++ tarea3dwes/src/main/java/com/pelayora/tarea3dwes/controlador/ViveroController.java
@@ -3,20 +3,18 @@ package com.pelayora.tarea3dwes.controlador;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
 @SessionAttributes("nombreUsuario")
 public class ViveroController {
 	
-	@GetMapping({"/", "/inicio"})
-	public String inicio(@ModelAttribute("nombreUsuario") String nombreUsuario, Model modelo) {
-		
-		modelo.addAttribute("UsuarioActual", nombreUsuario);
-        modelo.addAttribute("InicialUsuario", obtenerPrimeraLetra(nombreUsuario));
-        return "inicio";
+	@GetMapping("/anadirpersonal")
+	public String añadirPersonal(Model model) {
+	    model.addAttribute("mensaje", "Añadir Personal");
+	    return "añadirpersonal";
 	}
+
 	
 	// Método para obtener la primera letra
     private String obtenerPrimeraLetra(String nombre) {
--- tarea3dwes/src/main/java/com/pelayora/tarea3dwes/principal/Principal.java
+++ tarea3dwes/src/main/java/com/pelayora/tarea3dwes/principal/Principal.java
@@ -24,6 +24,30 @@ public class Principal implements CommandLineRunner {
 		System.out.println("************************************************");
 
 		System.out.println("--Bienvenido al sistema del invernadero (DWES)--");
+		
+		 try {
+	            String url = "http://localhost:8080/anadirpersonal";
+	            System.out.println("\n\t\t\t\033[1;36mLink de Entrada --> " + url + "\033[0m");
+	            
+	            // Intenta abrir el navegador mediante el sistema operativo
+	            ProcessBuilder pb = new ProcessBuilder();
+	            String os = System.getProperty("os.name").toLowerCase();
+	            if (os.contains("win")) {
+	                pb.command("cmd", "/c", "start", url);
+	            } else if (os.contains("mac")) {
+	                pb.command("open", url);
+	            } else if (os.contains("nix") || os.contains("nux")) {
+	                pb.command("xdg-open", url);
+	            } else {
+	                throw new UnsupportedOperationException("Sistema operativo no soportado para abrir navegadores.");
+	            }
+	            pb.start();
+	            System.out.println("\n\t\t\t\033[1;32mAbriendo el navegador con el enlace...\033[0m");
+	        } catch (Exception e) {
+	            System.out.println("\n\t\t\t\033[1;31mError al intentar abrir el navegador:\033[0m");
+	            e.printStackTrace();
+	        }
+
 
 		facade.iniciosesion();
 	}
